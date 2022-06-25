d407190b7408399b5ab1020ad53be7efc1d706d0
trojnartom <trojnar.t@hotmail.com>
2022-06-25 21:52:54 +0200

    Insitution repo - annotation

--- src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
+++ src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
@@ -1,11 +1,11 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.coderslab.charity.entity.Institution;
 
 @Repository
 public interface InstitutionRepository extends JpaRepository <Institution, Long> {
-
 
 }
