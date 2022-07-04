79a680f07b44058e3b729dfba753a1f893e4f30e
trojnartom <trojnar.t@hotmail.com>
2022-07-04 16:55:30 +0200

    Donation Repository created

--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -1,8 +1,12 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.coderslab.charity.entity.Donation;
+import pl.coderslab.charity.entity.Institution;
+
+import java.util.List;
 
 @Repository
 public interface DonationRepository extends JpaRepository <Donation, Long> {
